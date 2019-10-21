137a7cfd34cc3b733b7458975c1eec69c5ec5424
Zazama <jan.metzger@gmx.net>
2019-10-21 15:57:42 +0200

    fix to correct verify mail receiver

--- src/Extensions/SubmittedFormExtension.php
+++ src/Extensions/SubmittedFormExtension.php
@@ -4,14 +4,9 @@ namespace Zazama\DoubleOptIn\Extensions;
 
 use SilverStripe\Control\Email\Email;
 use SilverStripe\ORM\DataExtension;
-use SilverStripe\UserForms\Model\Submission\SubmittedFormField;
 use Zazama\DoubleOptIn\Models\EmailVerification;
 
 class SubmittedFormExtension extends DataExtension {
-    private static $db = [
-        'SendVerification' => 'Boolean(0)'
-    ];
-
     private static $has_one = [
         'EmailVerification' => EmailVerification::class
     ];
@@ -30,37 +25,5 @@ class SubmittedFormExtension extends DataExtension {
         } else {
             return parent::canView($member);
         }
-    }
-
-    public function onBeforeWrite() {
-        if(!$this->owner->isInDb()) {
-            $this->owner->setField('SendVerification', '1');
-        } else {
-            $this->owner->setField('SendVerification', '0');
-        }
-
-        parent::onBeforeWrite();
-    }
-
-    public function onAfterWrite() {
-        if(!$this->owner->getField('SendVerification')) {
-            parent::onAfterWrite();
-            return;
-        }
-
-        if($this->owner->Parent() && $this->owner->Parent()->EnableDoubleOptIn && $this->owner->Parent()->DoubleOptInFieldID) {
-            $email = SubmittedFormField::get()->where(['Name' => $this->owner->Parent()->DoubleOptInField()->Name])->limit(1)[0];
-            if(!$email) {
-                return;
-            }
-            $emailVerification = EmailVerification::create();
-            $emailVerification->init($email->Value);
-            $this->owner->EmailVerificationID = $emailVerification->ID;
-            $emailVerification->SubmittedFormID = $this->owner->ID;
-            $emailVerification->send($this->owner->Parent()->DoubleOptInSubject);
-            $emailVerification->write();
-            $this->owner->write();
-        }
-        parent::onAfterWrite();
     }
 }
--- src/Extensions/UserDefinedFormControllerExtension.php
+++ src/Extensions/UserDefinedFormControllerExtension.php
@@ -5,10 +5,14 @@ namespace Zazama\DoubleOptIn\Extensions;
 use SilverStripe\Core\Extension;
 use Zazama\DoubleOptIn\Models\EmailDummy;
 use Zazama\DoubleOptIn\Models\UserFormEmailToSend;
+use Zazama\DoubleOptIn\Models\EmailVerification;
+use SilverStripe\UserForms\Model\Submission\SubmittedFormField;
 
 class UserDefinedFormControllerExtension extends Extension {
     public function updateEmail(&$email, $recipient, $emailData) {
-        $page = $emailData['Fields'][0]->Parent()->Parent();
+        $referenceField = $emailData['Fields'][0];
+        $submittedForm = $referenceField->Parent();
+        $page = $submittedForm->Parent();
         if($page->EnableDoubleOptIn && $page->DoubleOptInFieldID) {
             $emailToSend = UserFormEmailToSend::create();
             $emailToSend->setData($email, $recipient, $emailData);
@@ -17,6 +21,23 @@ class UserDefinedFormControllerExtension extends Extension {
             $email = EmailDummy::create();
         } else {
             return;
+        }
+
+        if($page && $page->EnableDoubleOptIn && $page->DoubleOptInFieldID) {
+            $emailField = SubmittedFormField::get()->filter([
+                'Name' => $page->DoubleOptInField()->Name,
+                'ParentID' => $submittedForm->ID
+            ])->limit(1)[0];
+            if(!$emailField) {
+                return;
+            }
+            $emailVerification = EmailVerification::create();
+            $emailVerification->init($emailField->Value);
+            $submittedForm->EmailVerificationID = $emailVerification->ID;
+            $emailVerification->SubmittedFormID = $submittedForm->ID;
+            $emailVerification->send($page->DoubleOptInSubject);
+            $emailVerification->write();
+            $submittedForm->write();
         }
     }
 }
